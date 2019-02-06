da03491fd59608834127f4a428521edba5afb2bf
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-06 20:51:45 +0000

    task performed

---
+++ task2.py
@@ -0,0 +1,19 @@
+#Use both Bus_Stops.csv and Street_Centerlines.csv to find out number of busstops that are
+
+f = open("Bus_Stops.csv", 'r')
+f = open("Street_Centerlines.csv", 'r')
+
+
+def accessible():
+
+
+
+
+def non_standard():
+
+
+
+def inaccessible():
+
+
+
